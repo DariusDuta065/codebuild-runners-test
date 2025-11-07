c9377b74236374e8e6d4234c019f8d4e0778cedb
Darius Duta <dduta065@gmail.com>
2025-11-07 09:12:00 +0000

    feat(docker): add Dockerfile for java-21 base image to ECR

--- infra/private-ecr-repo-example/upload-200mb-image.sh
+++ infra/private-ecr-repo-example/upload-200mb-image.sh
@@ -3,16 +3,21 @@ set -e
 
 ECR_REPO_URL=${1:-${ECR_REPO_URL:-590624982938.dkr.ecr.eu-west-1.amazonaws.com}}
 
-aws ecr get-login-password | docker login --username AWS --password-stdin $ECR_REPO_URL
-
 if [ -z "$ECR_REPO_URL" ]; then
   echo "Usage: $0 <ecr-repo-url>"
   echo "   or: ECR_REPO_URL=<ecr-repo-url> $0"
   exit 1
 fi
 
-docker pull public.ecr.aws/nginx/nginx:latest
-docker tag public.ecr.aws/nginx/nginx:latest ${ECR_REPO_URL}/200mb-image:latest
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+aws ecr get-login-password | docker login --username AWS --password-stdin $ECR_REPO_URL
+
+echo "Building Docker image from Dockerfile..."
+docker build -t ${ECR_REPO_URL}/200mb-image:latest -f ${SCRIPT_DIR}/Dockerfile ${SCRIPT_DIR}
+
+echo "Pushing image to ECR..."
 docker push ${ECR_REPO_URL}/200mb-image:latest
 
 echo "Successfully pushed 200mb-image to ${ECR_REPO_URL}"
